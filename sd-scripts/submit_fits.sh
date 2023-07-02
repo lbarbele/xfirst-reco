22ccc0fec72e2fb1db3045267b28f4d1afd6e480
lbarbele <luan.arbeletche@gmail.com>
2023-07-02 19:09:00 -0300

    feat: helper script to submit fitters on cluster

---
+++ sd-scripts/submit_fits.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+readonly -a min=(600 350 100 560 300 50 450 100 0)
+readonly -a max=(1000 1000 1000 1250 1250 1250 1750 1750 1750)
+readonly -a len="${#min[@]}"
+readonly exec=${SCRATCH}/ml-conex-xfirst/sd-scripts/make_fits.srm
+
+for i in $(seq 0 $((${len}-1)))
+do
+  echo sbatch ${exec} ${min[${i}]} ${max[${i}]}
+done
