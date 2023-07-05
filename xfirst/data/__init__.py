ef55ae890849b73685ce2868536fad05500fec09
lbarbele <luan.arbeletche@gmail.com>
2023-07-05 11:58:26 -0300

    refactor: do not use 'dir' word as it is a builtin

--- xfirst/data/__init__.py
+++ xfirst/data/__init__.py
@@ -1,5 +1,4 @@
-import collections
-from concurrent.futures import ProcessPoolExecutor
+import concurrent.futures
 import itertools
 import os
 import pathlib
@@ -28,14 +27,14 @@ def load_profiles(
   format: Literal['np', 'pd'] = 'np',
 ) -> dict[config.dataset_t, np.ndarray] | pd.DataFrame:
   
-  dir = pathlib.Path(datadir).resolve()
+  profdir = pathlib.Path(datadir).resolve()/'profiles'
   pts = [particles] if isinstance(particles, str) else particles
   dts = [datasets] if isinstance(datasets, str) else datasets
-  nsh = collections.defaultdict(lambda: None, {} if nshowers is None else dict(nshowers))
+  nsh = {} if nshowers is None else dict(nshowers)
   cut = config.cuts.get(cut)
 
   # read depths and determine depth cuts
-  depths = np.load(dir/'profiles/depths.npy')
+  depths = np.load(profdir/'depths.npy')
   il, ir = util.get_range(depths, cut.min_depth, cut.max_depth)
   columns = [f'Edep_{i}' for i in range(len(depths))][il:ir]
 
@@ -43,9 +42,9 @@ def load_profiles(
 
   for d in dts:
     # read data
-    data = {p: np.load(dir/f'profiles/{d}/{p}.npy', mmap_mode = 'r') for p in pts}
+    data = {p: np.load(profdir/f'{d}/{p}.npy', mmap_mode = 'r') for p in pts}
     # apply cuts
-    data = {prm: np.copy(v[:nsh[d], il:ir]) for prm, v in data.items()}
+    data = {prm: np.copy(v[:nsh.get(d), il:ir]) for prm, v in data.items()}
     # format data
     if format == 'np':
       ret.append(data if len(pts) > 1 else data[pts[0]])
@@ -61,22 +60,22 @@ def load_profiles(
   return ret if len(ret) > 1 else ret[0]
 
 def load_tables(
-  dir: str | os.PathLike,
+  path: str | os.PathLike,
   datasets: config.dataset_t | Sequence[config.dataset_t] = config.datasets,
   particles: config.particle_t | Sequence[config.particle_t] = config.particles,
   nshowers: dict[config.dataset_t, int] | None = None,
 ) -> pd.DataFrame | list[pd.DataFrame] :
   
-  dir = pathlib.Path(dir).resolve()
+  tabdir = pathlib.Path(path).resolve()
   pts = [particles] if isinstance(particles, str) else particles
   dts = [datasets] if isinstance(datasets, str) else datasets
-  nsh = collections.defaultdict(lambda: None, {} if nshowers is None else dict(nshowers))
+  nsh = {} if nshowers is None else dict(nshowers)
 
   ret = []
 
   for d in dts:
-    with ProcessPoolExecutor(1) as exec:
-      data = exec.map(util.parquet_load, [dir/d/p for p in pts], itertools.repeat(nsh[d], len(pts)))
+    with concurrent.futures.ProcessPoolExecutor(1) as exec:
+      data = exec.map(util.parquet_load, [tabdir/d/p for p in pts], itertools.repeat(nsh.get(d), len(pts)))
       data = pd.concat(data, keys = pts, copy = False)
       ret.append(data)
 
@@ -118,9 +117,8 @@ def make_fits(
   verbose: bool = True,
 ):
   
-  dir = pathlib.Path(datadir).resolve()
-  wrk = os.cpu_count() if workers is None else workers
-  nsh = collections.defaultdict(lambda: None, {} if nshowers is None else dict(nshowers))
+  basedir = pathlib.Path(datadir).resolve()
+  batches = os.cpu_count() if workers is None else workers
   fcn = profile_functions.usp
 
   util.echo(verbose, f'generating fits for cut configurations {[c.name for c in config.cuts]}')
@@ -134,21 +132,21 @@ def make_fits(
         datasets = d,
         particles = p,
         cut = c,
-        datadir = dir,
-        nshowers = nsh,
+        datadir = basedir,
+        nshowers = nshowers,
         return_depths = True,
         format = 'np',
       )
 
-      ys = util.split(profiles, batches = wrk)
+      ys = util.split(profiles, batches = batches)
       fs = [fcn() for _ in ys]
       xs = [itertools.repeat(np.copy(depths), len(y)) for y in ys]
 
-      with ProcessPoolExecutor(workers) as exec:
+      with concurrent.futures.ProcessPoolExecutor(batches) as exec:
         fits = exec.map(fcn.get_fits, fs, xs, ys)
         fits = np.concatenate(list(fits))
         fits = pd.DataFrame(fits, columns = fcn().columns, index = pd.Index(range(len(fits)), name = 'id'))
-        util.parquet_save(dir/f'fits/range-{c.min_depth}-{c.max_depth}/{d}/{p}', fits, verbose)
+        util.parquet_save(basedir/f'fits/range-{c.min_depth}-{c.max_depth}/{d}/{p}', fits, verbose)
 
 #
 # extract data from conex files 
@@ -159,11 +157,11 @@ def make_conex_split(
   nshowers: dict[config.dataset_t, int],
 ) -> dict[config.dataset_t, dict[config.particle_t, list[str]]]:
   
-  dir = pathlib.Path(datadir).resolve(strict = True)
+  basedir = pathlib.Path(datadir).resolve(strict = True)
   ret = {d: {} for d in config.datasets}
   
   for p in config.particles:
-    files = set(map(str, dir.glob(f'conex/{p}*/**/*.root')))
+    files = set(map(str, basedir.glob(f'conex/{p}*/**/*.root')))
     for d in config.datasets:
       ret[d][p] = conex.parser(files, [], nshowers[d]).files
       files -= set(ret[d][p])
@@ -177,12 +175,14 @@ def make_datasets(
   verbose: bool = True,
 ) -> None:
   
-  dir = pathlib.Path(datadir).resolve()
-  nsh = collections.defaultdict(lambda: None, {} if nshowers is None else dict(nshowers))
-  bcs = ('Xfirst', 'lgE')
+  basedir = pathlib.Path(datadir).resolve()
+  profdir = basedir/'profiles'
+  xfstdir = basedir/'xfirst'
+  branches = ('Xfirst', 'lgE')
+  nsh = {} if nshowers is None else dict(nshowers)
 
   # split conex files, if hasn't been done already
-  conexjson = dir/'conex.json'
+  conexjson = basedir/'conex.json'
   if not conexjson.exists() or refresh:
     util.echo(verbose, 'splitting conex files')
     conexpaths = make_conex_split(datadir, nshowers)
@@ -195,16 +195,16 @@ def make_datasets(
   util.echo(verbose, 'generating profile datasets')
 
   for d, p in itertools.product(config.datasets, config.particles):
-    data = conex.parser(files = conexpaths[d][p], branches = ['Edep'], nshowers = nsh[d], concat = True).get_table('np')
-    util.np_save(dir/'profiles'/d/p, data, verbose)
+    data = conex.parser(files = conexpaths[d][p], branches = ['Edep'], nshowers = nsh.get(d), concat = True).get_table('np')
+    util.np_save(profdir/d/p, data, verbose)
 
   # depths
   util.echo(verbose, '\nsaving slant depth profile')
   depths = conex.parser(files = conexpaths[d][p], branches = ['Xdep'], nshowers = 1, concat = True)[0]
-  util.np_save(dir/'profiles'/'depths', depths, verbose)
+  util.np_save(profdir/'depths', depths, verbose)
 
   # xfirst
   util.echo(verbose, '\ngenerating xfirst datasets')
   for d, p in itertools.product(config.datasets, config.particles):
-    data = conex.parser(files = conexpaths[d][p], branches = bcs, nshowers = nsh[d], concat = True).get_table('pd')
-    util.parquet_save(dir/'xfirst'/d/p, data, verbose)
+    data = conex.parser(files = conexpaths[d][p], branches = branches, nshowers = nsh.get(d), concat = True).get_table('pd')
+    util.parquet_save(xfstdir/d/p, data, verbose)
