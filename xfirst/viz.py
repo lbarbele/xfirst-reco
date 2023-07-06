d5d5139daea57d5f6580042a0477d083581daa7e
lbarbele <luan.arbeletche@gmail.com>
2023-07-06 00:25:30 -0300

    refactor: draw_fit_parameter doesnt call seaborn

--- xfirst/viz.py
+++ xfirst/viz.py
@@ -1,38 +1,108 @@
-import matplotlib.pyplot as plt
+from typing import Sequence
+
+import matplotlib.axes
+import matplotlib.figure
+import numpy as np
 import pandas as pd
 import seaborn as sns
 
+from . import config
 from . import profile_functions
 
-def draw_fit_parameters(data):
+def draw_fit_parameters(
+  data: pd.DataFrame,
+  fcn: profile_functions.profile_function = profile_functions.usp(),
+  axes: Sequence[matplotlib.axes.Axes] | None = None,
+  stat: bool = True,
+  pallete: str = 'rocket',
+  nbins: int = 50,
+  annot: bool = True,
+) -> matplotlib.figure.Figure:
+  
+  if hasattr(data.index, 'levels'):
+    particles = [p for p in config.particles if p in data.index.levels[0]]
+    npart = len(particles)
+  else:
+    particles = None
+    npart = 1
+  
+  ncols = fcn.npar + int(stat)
+  colors = sns.color_palette(pallete, npart + 1)
 
-  fcn = profile_functions.usp()
+  if axes is None:
+    fig, axes = matplotlib.pyplot.subplots(1, ncols, figsize = (4*ncols, 4))
+  else:
+    fig = axes[0].figure
 
-  fig, axes = plt.subplots(1, fcn.npar + 1, figsize = (20, 4))
-
-  nice_names = {
+  labels = {
     'lgNmax': '$\log_{10}\\left(\\frac{N_\mathrm{max}}{\mathrm{GeV}\, \mathrm{g}^{-1}\, \mathrm{cm}^2}\\right)$',
-    'Xmax': '$X_\mathrm{max}$ [g cm$^{-2}$]',
-    'L': 'L [g cm$^{-2}$]',
-    'R': 'R',
-    'stat': '$\chi^2/\mathrm{ndf}$'
+      'Xmax': '$X_\mathrm{max}$ [g cm$^{-2}$]',
+         'L': 'L [g cm$^{-2}$]',
+         'R': 'R',
+      'stat': '$\chi^2/\mathrm{ndf}$'
   }
 
-  draw_args = {
-    'multiple': 'stack',
-    'hue': 'particle',
-    'palette': 'rocket',
-    'edgecolor': '.3',
-    'linewidth': .5,
-    'bins': 50,
+  scales = {
+    'lgNmax': 'linear',
+      'Xmax': 'log',
+         'L': 'log',
+         'R': 'log',
+      'stat': 'log',
   }
 
-  for icol, param in enumerate(fcn.parameter_names):
-    ax = axes[icol]
-    sns.histplot(data = data, x = param, log_scale = (icol != 0, True), ax = ax, legend = False, **draw_args)
-    ax.set_xlabel(nice_names[param])
+  args = {
+    'stacked': True,
+    'log': True,
+    'label': particles,
+    'color': colors[1:],
+    'edgecolor': colors[0],
+    'linewidth': 0.5,
+    'alpha': 0.9,
+  }
 
-  stat = pd.DataFrame({'stat': data.loc[:, 'chi2']/data.loc[:, 'ndf']})
-  sns.histplot(data = stat, x = 'stat', log_scale = (True, True), ax = axes[-1], **draw_args)
+  def annotate(v, ax):
+    if not annot: return
+    ax.annotate(f'min: {v.min():.4g}', xy = (0.03, 0.95), xycoords = 'axes fraction', fontsize = 8)
+    ax.annotate(f'max: {v.max():.4g}', xy = (0.03, 0.90), xycoords = 'axes fraction', fontsize = 8)
+
+  def compute(param):
+    if param == 'stat':
+      values = data.loc[:, 'chi2']/data.loc[:, 'ndf']
+    else:
+      values = data.loc[:, param]
+
+    if scales[param] == 'log':
+      values = values.loc[values > 0]
+      bins = 10**np.linspace(np.log10(values.min()), np.log10(values.max()), nbins + 1)
+    else:
+      bins = np.linspace(values.min(), values.max(), nbins + 1)
+
+    return values, bins
+  
+  def split(v):
+    if npart > 1:
+      return [v.loc[p] for p in particles]
+    else:
+      return v
+
+  for ax, param in zip(axes, fcn.parameter_names):
+    values, bins = compute(param)
+    ax.hist(split(values), bins = bins, **args)
+    ax.set_xscale(scales[param])
+    ax.set_xlabel(labels[param])
+    ax.set_ylabel('Count')
+    annotate(values, ax)
+
+  if stat is not None:
+    ax = axes[-1]
+    values, bins = compute('stat')
+    ax.hist(split(values), bins = bins, **args)
+    ax.set_xscale(scales['stat'])
+    ax.set_xlabel(labels['stat'])
+    ax.set_ylabel('Count')
+    annotate(values, ax)
+  
+  if npart > 1:
+    axes[-1].legend()
 
   fig.tight_layout()
