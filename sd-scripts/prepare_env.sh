fa24c59f67312d484110729723c7ba5851ec797f
lbarbele <luan.arbeletche@gmail.com>
2023-07-11 10:34:14 -0300

    config: setup number of showers and filter on sd

--- sd-scripts/prepare_env.sh
+++ sd-scripts/prepare_env.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# helper functions
+# *
+# * helper functions
+# *
 
 function redirect_std()
 {
@@ -22,7 +24,9 @@ function set_job_name() {
 }
 export -f set_job_name
 
-# directories
+# *
+# * directories
+# *
 
 export HOME_DIR='/scratch/astroparti/luan.arbeletche'
 export BASE_DIR="${HOME_DIR}/ml-conex-xfirst"
@@ -32,17 +36,29 @@ export DATA_DIR="${BASE_DIR}/data"
 export MODELS_DIR="${BASE_DIR}/models"
 export SCRIPTS_DIR="${BASE_DIR}/scripts"
 
+# *
+# * configuration affecting results
+# *
+
+# filter to apply when loading the datasets
+
+export LOAD_FILTER='lgE >= 18'
+
 # number of showers to load from conex files
 
-export MAX_TRAIN=1800000
-export MAX_VAL=1800000
-export MAX_TEST=400000
+export LOAD_TRAIN=1606308
+export LOAD_VAL=1606308
+export LOAD_TEST=803154
 
 # number of showers to use on traning and evaluation
 
 export NTRAIN=1000000
 export NVAL=1000000
-export NTEST=300000
+export NTEST=500000
+
+# *
+# * environment setup
+# *
 
 # rename job
 
@@ -54,37 +70,42 @@ fi
 
 redirect_std
 
-# environment
-
 # load modules
+
 module load cmake/3.17.3
 module load binutils/2.32
 module load cudnn/8.2_cuda-11.4
 module load gcc/11.1
 
 # python version
+
 export PYTHON_VERSION=3.11.3
 export PYTHON_MAJOR=3
 
 # python directories
+
 export PYTHON_TARGET=${SCRATCH}/programas/Python-${PYTHON_VERSION}
 export PYTHON_SRC=${PYTHON_TARGET}-src
 export PYTHON_PACKAGES=${PYTHON_TARGET}-packages
 export PYTHON_DEP=${PYTHON_TARGET}-dep
 
 # openssl
+
 export OPENSSL_TARGET=${PYTHON_DEP}/openssl-3.1.1
 export PATH=${OPENSSL_TARGET}/bin:${PATH}
 export LD_LIBRARY_PATH=${OPENSSL_TARGET}/lib64:${LD_LIBRARY_PATH}
 
 # root
+
 source /scratch/astroparti/luan.arbeletche/programas/root-6.28.02-python3.11.3/bin/thisroot.sh
 
 # update paths
+
 export PATH=${PYTHON_TARGET}/bin:${PATH}
 export LD_LIBRARY_PATH=${PYTHON_TARGET}/lib:${LD_LIBRARY_PATH}
 export PYTHONPATH="${BASE_DIR}:${PYTHON_PACKAGES}:${PYTHONPATH}"
 
 # aliases
+
 alias python=python3
 alias pip=pip3
