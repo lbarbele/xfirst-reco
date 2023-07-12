57234c2d115c70118452c35aa1bfec0512d49f7b
lbarbele <luan.arbeletche@gmail.com>
2023-07-12 17:49:13 -0300

    feat: scripts to train mlp in fut

--- scripts/train_mlp_fit.py
+++ scripts/train_mlp_fit.py
@@ -1,4 +1,5 @@
 import os
+import re
 from typing import Mapping, Sequence
 
 import click
@@ -7,18 +8,21 @@ import xfirst
 
 @click.command()
 @click.option('--datadir', type = click.Path(exists = True, dir_okay = True), required = True)
+@click.option('--layers', type = str, required = True)
 @click.option('--save', type = click.Path(), required = False, default = None)
 @click.option('--cuts', type = str, required = False, multiple = False, default = xfirst.config.cut.names())
 @click.option('--nshowers', type = (str, click.IntRange(1, None)), required = True, multiple = True)
 @click.option('--verbose/--no-verbose', default = True)
 def main(
   datadir: str | os.PathLike,
+  layers: str,
   save: str | os.PathLike | None = None,
   cuts: str | Sequence[str] = xfirst.config.cut.names(),
   nshowers: Mapping[xfirst.config.dataset_t, int] | int | None = None,
   verbose: bool = True,
 ):
   
+  layers = re.sub(',|-|\.|\/', ':', layers).split(':')
   cuts = [xfirst.config.cut.get(c) for c in xfirst.util.strlist(cuts)]
   
   x = xfirst.profile_functions.usp().parameter_names
@@ -41,16 +45,19 @@ def main(
       verbose  = verbose,
     )
 
-    # fit the gradient boosting regressor
-    model = xfirst.models.gradient_boosting_regressor(
-      verbose = verbose
+    # fit the multilayer perceptron
+    model = xfirst.models.multilayer_perceptron_regressor(
+      input = len(x),
+      layers = layers,
+      verbose = verbose,
+      epochs = 10,
     ).fit(data, x, y)
 
     # save model
     if save is not None:
       model.eval(
         data = data,
-        save = cut.path(f'{save}/xgb-fit'),
+        save = cut.path(f'{save}/mlp-fit-' + '-'.join(map(str, layers))),
         plot = True
       )
 
