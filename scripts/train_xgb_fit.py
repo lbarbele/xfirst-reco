ca12b041f15a9f2c9c4327d7c17e4c7298d2b54a
lbarbele <luan.arbeletche@gmail.com>
2023-07-12 14:18:00 -0300

    refactor: complete interface to train xgb on fit

--- scripts/train_xgb_fit.py
+++ scripts/train_xgb_fit.py
@@ -8,73 +8,49 @@ import xfirst
 @click.command()
 @click.option('--datadir', type = click.Path(exists = True, dir_okay = True), required = True)
 @click.option('--save', type = click.Path(), required = False, default = None)
-@click.option('--cuts', type = str, required = False, multiple = True, default = xfirst.config.cut_names)
-@click.option('--nshowers', type = (str, click.IntRange(1, None)), required = False, multiple = True)
+@click.option('--cuts', type = str, required = False, multiple = False, default = xfirst.config.cut.names())
+@click.option('--nshowers', type = (str, click.IntRange(1, None)), required = True, multiple = True)
 @click.option('--verbose/--no-verbose', default = True)
 def main(
   datadir: str | os.PathLike,
   save: str | os.PathLike | None = None,
-  cuts: Sequence[str] = xfirst.config.cut_names,
+  cuts: str | Sequence[str] = xfirst.config.cut.names(),
   nshowers: Mapping[xfirst.config.dataset_t, int] | int | None = None,
   verbose: bool = True,
 ):
   
-  cutlist = [xfirst.config.get_cut(c) for c in cuts]
-  features = xfirst.profile_functions.usp().parameter_names
-  target = 'Xfirst'
+  cuts = [xfirst.config.cut.get(c) for c in xfirst.util.strlist(cuts)]
+  
+  x = xfirst.profile_functions.usp().parameter_names
+  y = 'Xfirst'
+  
+  xfirst.util.echo(verbose, f'training on cut configurations {[c.name for c in cuts]}')
 
-  ret = {}
-
-  xfirst.util.echo(verbose, f'training on cut configurations {[c.name for c in cutlist]}')
-
-  for cut in cutlist:
+  for cut in cuts:
     xfirst.util.echo(verbose, f'\nprocessing cut configuration {cut.name}')
-    xfirst.util.echo(verbose, '+ loading data')
 
-    # load data
-    train, validation, test, normalization = xfirst.data.load_fits(
-      datadir = datadir,
-      cut = cut,
-      columns = features,
-      norm = features,
+    # load normalized data. drop bad fits on train and validation sets
+    data = xfirst.data.load_fits(
+      datadir  = datadir,
+      cut      = cut,
+      columns  = x,
+      xfirst   = True,
+      norm     = x,
+      drop_bad = {'train': True, 'validation': True, 'test': False},
       nshowers = nshowers,
-      drop = {'train': True, 'validation': True, 'test': False},
-      xfirst = True,
+      verbose  = verbose,
     )
 
-    xfirst.util.echo(verbose, '+ training the gradient boosting regressor')
+    # fit the gradient boosting regressor and add to return dict
+    model = xfirst.models.gradient_boosting_regressor().fit(data, x, y)
 
-    # fit a gradient boosting regressor and add to return dict
-    ret[cut.name] = xfirst.models.gradient_boosting_regressor().fit(
-      train = (train[features], train[target]),
-      validation = (validation[features], validation[target]),
-      verbose = verbose,
-    )
-
+    # save model
     if save is not None:
-      
-      # save model
-      outdir = ret[cut.name].save(f'{save}/xgb/range-{cut.min_depth}-{cut.max_depth}')
-
-      # compute and save predictions
-      xfirst.util.echo(verbose, f'+ computing predictions')
-      
-      results = ret[cut.name].test(test[features], test[target])
-      results['lgE'] = test['lgE']
-      results['good'] = test['good']
-
-      for p in results.index.levels[0]:
-        xfirst.util.parquet_save(outdir/'predictions'/p, results.loc[p])
-
-      # create plots and save
-      xfirst.viz.draw_predictions(results.loc[results['good']]).savefig(outdir/'predictions.pdf')
-
-      # save normalization
-      xfirst.util.parquet_save(outdir/'normalization', normalization)
-
-      xfirst.util.echo(verbose, f'+ model saved to {outdir}')
-
-  return ret if len(ret) > 1 else next(iter(ret.values()))
+      model.eval(
+        data = data,
+        save = f'{save}/xgb/range-{cut.min_depth}-{cut.max_depth}',
+        plot = True
+      )
 
 if __name__ == '__main__':
   main()
--- xfirst/models.py
+++ xfirst/models.py
@@ -1,103 +1,242 @@
 import abc
 import os
 import pathlib
-from typing import Literal
+from typing import overload, Any, Literal, Mapping, Self, Sequence
 
-import matplotlib
+import matplotlib.axes
+import matplotlib.figure
+import matplotlib.pyplot
 import numpy as np
 import pandas as pd
 import xgboost
 
+from . import config
 from . import util
 from . import viz
 
 # *
-# * model classes
+# * model interface
 # *
 
 class model(abc.ABC):
-  @abc.abstractmethod
-  def draw(self):
-    ...
-
-  @abc.abstractmethod
-  def fit(self, train, validation, verbose):
-    ...
-
-  @abc.abstractmethod
-  def predict(self, x):
-    ...
-
-  @abc.abstractmethod
-  def save(self, path: str | os.PathLike) -> pathlib.Path:
+  def __init__(
+    self,
+    verbose: bool = False,
+  ) -> None:
     
-    # create output directory
+    self._features = None
+    self._target = None
+    self._verbose = verbose
+
+  @property
+  def features(self):
+    return self._features
+  
+  @property
+  def target(self):
+    return self._target
+
+  @property
+  def verbose(self):
+    return self._verbose
+  
+  # draw validation curve
+
+  @abc.abstractmethod
+  def _draw(
+    self,
+    ax: matplotlib.axes.Axes
+  ) -> None:
+    ...
+
+  def draw(
+    self,
+    ax: matplotlib.axes.Axes | None = None
+  ) -> matplotlib.figure.Figure:
+
+    if ax is None:
+      fig = matplotlib.pyplot.figure()
+      ax = fig.gca()
+    else:
+      fig = ax.figure
+    
+    self._draw(ax)
+
+    return fig
+  
+  # evaluate model, plot, optionally save
+
+  def eval(
+    self,
+    data: Mapping[config.dataset_t | Literal['normalization'], pd.DataFrame], 
+    save: str | os.PathLike | None = None,
+    plot: bool = False,
+  ) -> pd.DataFrame:
+
+    # compute predictions
+    results = {}
+    results['predictions'] = self.predict(data['test'][self.features])
+    results['target'] = data['test'][self.target]
+    results['residuals'] = results['predictions'] - results['target']
+    results['lgE'] = data['test']['lgE']
+    results['good'] = data['test']['status'] > 0.99
+    results = pd.DataFrame(results)
+
+    # draw predictions
+    if plot is True:
+      util.echo(self.verbose, '+ drawing predictions')
+      goodresults = results.loc[results['good']]
+      fig = viz.draw_predictions(goodresults)
+    
+    if save is not None:
+      # save model
+      outdir = self.save(save)
+      # save predictions
+      for p in results.index.levels[0]:
+        res = results.loc[p]
+        util.hdf_save(outdir/'predictions', data = res, key = p, verbose = self.verbose)
+      # save plot
+      if plot is True:
+        fig.savefig(outdir/'predictions.pdf')
+      # save normalization
+      norm = data['normalization']
+      util.hdf_save(outdir/'normalization', data = norm, key = 'normalization', verbose = self.verbose)
+
+    return results
+
+  # fit model
+
+  @abc.abstractmethod
+  def _fit(
+    self,
+    train: tuple[pd.DataFrame, pd.Series],
+    validation: tuple[pd.DataFrame, pd.Series],
+  ) -> None:
+    ...
+
+  def fit(
+    self,
+    data: Mapping[config.dataset_t, pd.DataFrame],
+    x: str | Sequence[str],
+    y: str,
+  ) -> Self:
+    
+    self._features = x
+    self._target = y
+    
+    util.echo(self.verbose, f'+ training the model on {self.features} for target {self.target}')
+
+    train = (data['train'][x], data['train'][y])
+    valid = (data['validation'][x], data['validation'][y])
+    self._fit(train, valid)
+
+    return self
+  
+  # predict on test data
+
+  @abc.abstractmethod
+  def _predict(
+    self,
+    x: pd.DataFrame
+  ) -> np.ndarray:
+    ...
+
+  def predict(
+    self,
+    data: pd.DataFrame | Mapping[config.dataset_t, pd.DataFrame],
+  ) -> np.ndarray:
+    
+    util.echo(self.verbose, f'+ computing predictions')
+
+    x = data if isinstance(data, pd.DataFrame) else data['train']
+    x = x[self.features]
+
+    y = self._predict(x)
+    y = pd.Series(y, index = x.index)
+    return y
+
+  # save model
+
+  @abc.abstractmethod
+  def _save(
+    self,
+    path: pathlib.Path,
+  ) -> pathlib.Path:
+    ...
+
+  def save(
+    self,
+    path: str | os.PathLike,
+  ) -> pathlib.Path:
+    
     outdir = pathlib.Path(path).resolve()
     os.makedirs(outdir, exist_ok = True)
 
-    # save the validation curve
-    self.draw().savefig(outdir/'validation_curve.pdf')
+    self._save(outdir)
+    util.echo(self.verbose, f'+ model saved under {outdir}')
 
-    # return path object
+    fig = self.draw()
+    fig.savefig(outdir/'validation_curve.pdf')
+    matplotlib.pyplot.close(fig)
+    util.echo(self.verbose, f'+ validation curve saved to {outdir}/validation_curve.pdf')
+
     return outdir
 
-  def test(self, x, y, format: Literal['np', 'pd'] = 'pd'):
-    
-    if len(x) != len(y):
-      raise ValueError('model.test: x and y have different lengths')
-    
-    results = np.zeros(shape = (len(x), 3), dtype = np.float32)
-    results[:, 0] = self.predict(x)
-    results[:, 1] = y
-    results[:, 2] = results[:, 0] - results[:, 1]
-
-    if format == 'np':
-      return results
-    elif format == 'pd':
-      cols = ['predictions', 'target', 'residuals']
-      idx = x.index if isinstance(x, pd.DataFrame) else y.index if isinstance(y, pd.DataFrame) else None
-      return pd.DataFrame(results, columns = cols, index = idx)
-    else:
-      raise ValueError(f'model.test: invalid format {format}')
+# *
+# * XGboost wrapper
+# *
 
 class gradient_boosting_regressor(model):
-  def __init__(self, **kwargs):
-
+  def __init__(
+    self,
+    verbose: bool = True,
+    **kwargs
+  ) -> None:
+    
+    super().__init__(verbose = verbose)
+    
     kwargs.setdefault('n_estimators', 1000)
     kwargs.setdefault('n_jobs', os.cpu_count())
     kwargs.setdefault('early_stopping_rounds', 7)
 
     self._xgb = xgboost.XGBRegressor(**kwargs)
 
-  def draw(self, ax: matplotlib.axes.Axes | None = None) -> matplotlib.figure.Figure:
-    ytrain = self.xgb.evals_result()['validation_0']['rmse']
-    yval = self.xgb.evals_result()['validation_1']['rmse']
-    x = np.arange(len(ytrain)) + 1
+  def _draw(
+    self,
+    ax: matplotlib.axes.Axes,
+  ) -> None:
 
-    if ax is None:
-      fig = matplotlib.pyplot.figure()
-      ax = fig.gca()
+    yt = self.xgb.evals_result()['validation_0']['rmse']
+    yv = self.xgb.evals_result()['validation_1']['rmse']
+    x = np.arange(len(yt)) + 1
 
-    ax.plot(x, yval, '-', label = 'Validation', color = 'navy', alpha = 0.7)
-    ax.plot(x, ytrain, '--', label = 'Train', color = 'orange')
-    ax.legend()
+    ax.plot(x, yv, '-', label = 'Validation', color = 'navy', alpha = 0.7)
+    ax.plot(x, yt, '--', label = 'Train', color = 'orange')
     ax.set_ylabel('Root mean squared error')
     ax.set_xlabel('$n_\mathrm{trees}$')
-
-    return ax.figure
-
-  def fit(self, train: tuple, validation: tuple, verbose: bool = True) -> model:
-    self.xgb.fit(X = train[0], y = train[1], eval_set = [train, validation], verbose = verbose)
-    return self
+    ax.legend()
   
-  def predict(self, x):
-    return self.xgb.predict(X = x)
-  
-  def save(self, path: str | os.PathLike) -> pathlib.Path:
+  def _fit(
+    self,
+    train: tuple[pd.DataFrame, pd.Series],
+    validation: tuple[pd.DataFrame, pd.Series],
+  ) -> Self:
     
-    p = super().save(path)
-    self.xgb.save_model(p/'model.ubj')
-    return p
+    self.xgb.fit(*train, eval_set = [train, validation], verbose = self.verbose)
+  
+  def _predict(
+    self,
+    x: pd.DataFrame,
+  ) -> np.ndarray:
+    
+    return self.xgb.predict(x)
+  
+  def _save(
+    self,
+    path: pathlib.Path,
+  ) -> None:
+    
+    self.xgb.save_model(path/'model.ubj')
 
   @property
   def xgb(self):
