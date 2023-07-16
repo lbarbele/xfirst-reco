11e823c22a79966a23fb426ccd3407381d0080c1
luan.arbeletche <luan.arbeletche@gmail.com>
2023-07-16 18:01:21 -0300

    config: sd script submit themselves by being exec

--- sd-scripts/prepare_env.sh
+++ sd-scripts/prepare_env.sh
@@ -6,23 +6,34 @@
 
 function redirect_std()
 {
+  mkdir -p "${LOGS_DIR}"
+  
   tty -s || {
-    mkdir -p "${LOGS_DIR}"
     local -r base_path="${LOGS_DIR}/${SLURM_JOB_NAME}_job_${SLURM_JOB_ID:-0}${SLURM_STEP_ID:+_step_${SLURM_STEP_ID}_proc_${SLURM_PROCID}}"
-    exec 1> "${base_path}.out"
-    exec 2> "${base_path}.err"
+    exec 1>> "${base_path}.out"
+    exec 2>> "${base_path}.err"
   }
 }
 export -f redirect_std
 
 function set_job_name() {
-  local jobName="${1}"
-  [ -z "${jobName}" ] && return 0
-
-  scontrol update job "${SLURM_JOB_ID}" JobName="${jobName}"
-  export SLURM_JOB_NAME="${jobName}"
+  [ -z "${1}" ] && return 0
+  export SLURM_JOB_NAME="${1}"
+  [ -z "${SLURM_JOB_ID}" ] && return 0
+  scontrol update job "${SLURM_JOB_ID}" JobName="${1}"
 }
 export -f set_job_name
+
+function submit() {
+  [ -n "${SLURM_JOB_ID}" ] && return 0
+  if [ -n "${SLURM_JOB_NAME}" ]; then
+    sbatch -J "${SLURM_JOB_NAME}" -o "${LOGS_DIR}/%x_job_%J.out" -e "${LOGS_DIR}/%x_job_%J.err" "${0}" "${@}"
+  else
+    sbatch -o "${LOGS_DIR}/%x_job_%J.out" -e "${LOGS_DIR}/%x_job_%J.err" "${0}" "${@}"
+  fi
+  exit 0
+}
+export -f submit
 
 # *
 # * directories
@@ -60,52 +71,58 @@ export NTEST=500000
 # * environment setup
 # *
 
-# rename job
+if [ -z "${HAS_ENV}" ]; then
 
-if [ -n "${RENAME_JOB}" ]; then
-  set_job_name "${RENAME_JOB}"
+  # rename job
+
+  if [ -n "${RENAME_JOB}" ]; then
+    set_job_name "${RENAME_JOB}"
+  fi
+
+  # redirect output to the joblogs dir
+
+  redirect_std
+
+  # load modules
+
+  module load cmake/3.17.3
+  module load binutils/2.32
+  module load cudnn/8.2_cuda-11.4
+  module load gcc/11.1
+
+  # python version
+
+  export PYTHON_VERSION=3.11.3
+  export PYTHON_MAJOR=3
+
+  # python directories
+
+  export PYTHON_TARGET=${SCRATCH}/programas/Python-${PYTHON_VERSION}
+  export PYTHON_SRC=${PYTHON_TARGET}-src
+  export PYTHON_PACKAGES=${PYTHON_TARGET}-packages
+  export PYTHON_DEP=${PYTHON_TARGET}-dep
+
+  # openssl
+
+  export OPENSSL_TARGET=${PYTHON_DEP}/openssl-3.1.1
+  export PATH=${OPENSSL_TARGET}/bin:${PATH}
+  export LD_LIBRARY_PATH=${OPENSSL_TARGET}/lib64:${LD_LIBRARY_PATH}
+
+  # root
+
+  source /scratch/astroparti/luan.arbeletche/programas/root-6.28.02-python3.11.3/bin/thisroot.sh
+
+  # update paths
+
+  export PATH=${PYTHON_TARGET}/bin:${PATH}
+  export LD_LIBRARY_PATH=${PYTHON_TARGET}/lib:${LD_LIBRARY_PATH}
+  export PYTHONPATH="${BASE_DIR}:${PYTHON_PACKAGES}:${PYTHONPATH}"
+
+  # aliases
+
+  alias python=python3
+  alias pip=pip3
+
+  export HAS_ENV=1
+
 fi
-
-# redirect output to the joblogs dir
-
-redirect_std
-
-# load modules
-
-module load cmake/3.17.3
-module load binutils/2.32
-module load cudnn/8.2_cuda-11.4
-module load gcc/11.1
-
-# python version
-
-export PYTHON_VERSION=3.11.3
-export PYTHON_MAJOR=3
-
-# python directories
-
-export PYTHON_TARGET=${SCRATCH}/programas/Python-${PYTHON_VERSION}
-export PYTHON_SRC=${PYTHON_TARGET}-src
-export PYTHON_PACKAGES=${PYTHON_TARGET}-packages
-export PYTHON_DEP=${PYTHON_TARGET}-dep
-
-# openssl
-
-export OPENSSL_TARGET=${PYTHON_DEP}/openssl-3.1.1
-export PATH=${OPENSSL_TARGET}/bin:${PATH}
-export LD_LIBRARY_PATH=${OPENSSL_TARGET}/lib64:${LD_LIBRARY_PATH}
-
-# root
-
-source /scratch/astroparti/luan.arbeletche/programas/root-6.28.02-python3.11.3/bin/thisroot.sh
-
-# update paths
-
-export PATH=${PYTHON_TARGET}/bin:${PATH}
-export LD_LIBRARY_PATH=${PYTHON_TARGET}/lib:${LD_LIBRARY_PATH}
-export PYTHONPATH="${BASE_DIR}:${PYTHON_PACKAGES}:${PYTHONPATH}"
-
-# aliases
-
-alias python=python3
-alias pip=pip3
